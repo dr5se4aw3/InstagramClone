0c5b04e3ecbefc7756b32013b494483582faace7
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-02-18 11:39:52 +0900

    リンクの設定を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,15 +26,12 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to new_session_path
+    else
+      flash.now[:danger] = "登録に失敗しました。"
+      render :new
     end
   end
 
